eee92aaf5ebce09307ab95fe6ea274bfeb0d04df
imjord <dean.jay.goat@gmail.com>
2021-05-22 14:22:37 -0600

    updated inputs and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-var formEl = document.querySelector("#task-form"); 
-var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function(event) { 
-  event.preventDefault(); 
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-  var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
-  }; 
 
-  formEl.addEventListener("submit", createTaskHandler);
+var createTaskHandler = function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = ("task-item");
+  listItemEl.textContent = ("This is a new item created from javascript");
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+buttonEl.addEventListener("click", createTaskHandler);
