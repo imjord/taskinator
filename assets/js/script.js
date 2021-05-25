7fa05347d4bf5588d5c644c2ef1026600d537bff
imjord <dean.jay.goat@gmail.com>
2021-05-25 09:34:25 -0600

    added reset and validation for form

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,15 @@ var taskFormHandler = function(event) {
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
   
+
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+
+  formEl.reset();
+
+
    // package up data as an object
    var taskDataObj = {
     name: taskNameInput,
@@ -16,6 +25,7 @@ var taskFormHandler = function(event) {
 
   // send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
+  
 };
 
 
@@ -37,6 +47,8 @@ var createTaskEl = function(taskDataObj) {
 
  // add entire list item to list 
  tasksToDoEl.appendChild(listItemEl);
+
+
   
 
 }
